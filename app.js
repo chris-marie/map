2d1517f1b090e0127534bfb5e883628a5bb8a8f8
chris-marie <cmhamilton2.718@gmail.com>
2015-03-06 14:50:15 -0800

    stuffs

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.use('/', routes);
 app.use('/users', users);
 app.use('/map', map);
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
--- routes/map.js
+++ routes/map.js
@@ -17,7 +17,7 @@ router.get('/mark', function(req, res) {
     res.render('markMap', { title: 'Mark Map' });
 });
 
-router.get('/input', function(req, res) {
+router.get('/mapinput', function(req, res) {
     res.render('mapInput', { title: 'Input Map' });
 });
 
@@ -28,34 +28,61 @@ mongoose.connect('mongodb://localhost/test', function (err) {
     else console.log('Successfully connected to mongodb ... ');
 });
 
-var LocalModel = mongoose.model('Contact', {
+var LocalModel = mongoose.model('Local', {
     name: String,
     keyword: String,
-    latLng: String
+    lat: String,
+    lng: String,
+    city: String,
+    state: String,
+    zip: String
 });
 
-router.post('/contacts', function (req, res) {
+LocalModel.find(function (err, result) {
+    console.log(result);
+});
+router.get('/mapinput/:zip', function(req, res) {
+
+    var zipcode = req.params.zip;
+
+    LocalModel.find({ 'zip': zipcode},function (err, result) {
+        if (err) { res.status(500).json({ message: 'Sorry! Something broke on GET!' });}
+        else {
+            res.status(200).json(result);
+        }
+    });
+});
+
+router.post('/mapinput', function (req, res) {
     (new LocalModel(req.body)).save(function (err, result) {
-        if (err) res.status(500).json({ message: 'Sorry! Something broke!' });
+        if (err) res.status(500).json({ message: 'Sorry! Something broke on POST!' });
         else res.status(201).json(result)
     });
 });
-router.get('/contacts', function(req, res) {
-    LocalModel.find(function (err, result) {
-        if (err) res.status(500).json({ message: 'Sorry! Something broke!' });
-        else res.status(201).json(result)
-    });
-});
-router.put('/contacts/:id', function (req, res) {
+
+router.put('/mapinput/:id', function (req, res) {
     LocalModel.findByIdAndUpdate(req.params.id, req.body, function (err, result){
-        if (err) res.status(500).json({ message: 'Sorry! Something broke!' });
+        if (err) res.status(500).json({ message: 'Sorry! Something broke on PUT!' });
         else res.status(200).json(result);
     });
 });
-router.delete('/contacts/:id', function (req, res) {
+router.delete('/mapinput/:id', function (req, res) {
     LocalModel.findByIdAndRemove(req.params.id, function (err, result){
-        if (err) res.status(500).json({ message: 'Sorry! Something broke!' });
+        if (err) res.status(500).json({ message: 'Sorry! Something broke on DELETE!' });
         else res.status(200).json(result);
     });
 });
 module.exports = router;
+
+/*
+ * router.get('/mapinput/:keyword', function(req, res) {
+ var keywd = req.params[0].keyword;
+
+ LocalModel.find({ 'keyword.value': keywd},function (err, result) {
+ if (err) { res.status(500).json({ message: 'Sorry! Something broke on GET!' });}
+ else {
+ res.status(201).json(result);
+ }
+ });
+ });
+ */
